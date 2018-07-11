936a4c080cddab6a24020670e8fe32c5d0ce233a
Kumar <piykumar@deloitte.com>
2018-07-11 11:15:24 +0530

    first commit

---
+++ DependencyInjection/DependencyInject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class DependencyInject
+    {
+        private IAnimal obj;
+        public DependencyInject(IAnimal obj)
+        {
+            this.obj = obj;
+        }
+
+        public void print() 
+        {
+            obj.print();
+        }
+    }
+}
